1866e4f078c05d01e76462658c0c62e9370f63d3
janeJeong <jyoon2005@naver.com>
2021-08-30 00:30:10 +0900

    JavaScript_base_(0825)

---
+++ Web Programming/js-start/js/main.js
@@ -0,0 +1,242 @@
+// comic 박스 생성
+// 변수 선언
+// var comic;
+
+// comic 박스 안에 원피스 데이터를 할당한 상태
+// 변수 초기화
+// comic = "원피스";
+
+// console.log(comic);
+
+
+//변수 선언과 동시에 초기화 진행
+// var apple = "사과"; 
+
+
+/*
+var a;
+var b;
+var c;
+*/
+
+
+// 변수 동시 선언
+// var a, b, c;
+
+
+// 변수 동시 초기화
+/*
+var d = 10;
+var e = 20;
+var f = 30; 
+*/
+
+// var d = 10, e = 20, f = 30;
+
+
+
+
+
+// 모든 변수는 선언과 동시에 undefined 자동으로 할당된다.
+// var computer;
+// console.log(computer);
+
+
+/*
+var house;
+house = "집";
+console.log(house);
+
+house = "아파트";
+console.log(house);
+*/
+
+
+
+/*
+var house;
+console.log(house);
+
+//undefined -> 집
+house = "집"
+console.log(house);
+*/
+
+
+
+// 캐멀 케이스
+// var mathScoreStudent = 10;
+
+// 스네이크 케이스
+// var math_score_Student = 10;
+
+
+
+//<변수명 작성시 주의점>
+// var math-score-student = 100;
+
+// var _math;
+// var $math;
+
+// var apple10; (o)
+// var 10apple; (x)
+
+
+
+
+
+//데이터 타입
+//문자열 (String)
+// var msg1 = "Hellow World";
+// var msg2 = 'Welcome';
+
+// console.log(msg1);
+// console.log(msg2);
+
+// var msg3 = "Nice to meet you';"; (혼용 x)
+
+// She is girl
+// She's girl
+
+// var msg4 = "She's girl";
+// var msg5 = 'She\'s girl';
+
+// console.log(msg4);
+// console.log(msg5);
+
+
+// 숫자 (Number)
+// var num1 = 10;
+// var	num2 = -10;
+// var num3 = 3.14;
+// var num4 = -3.14;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+// console.log(num1 + num2);
+// console.log(num1 + num3);
+
+
+// 연산자
+// 숫자 사칙연산
+// var a = 20;
+// var b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// 문자열
+// var str1 = "20";
+// var str2 = "10";
+
+// console.log(str1 + str2);
+// console.log(str1 - str2);
+// console.log(str1 * str2);
+// console.log(str1 / str2);
+// console.log(str1 % str2);
+
+
+// var firstName = "Jane"
+// var lastName = "Jeong"
+
+// console.log(firstName + "       " + lastName)
+
+
+
+// var str11 = "현재 시간은 ";
+// var time = 10;
+// var str12 = "시 입니다.";
+
+// console.log(str11 + time + str12);
+
+
+
+
+// ++, -- 연산자
+// var num10 = 10;
+
+// num10 = num10 - 1;
+// console.log(num10);
+// console.log(--num10);  -> 9
+// console.log(--num10);  -> 8
+
+// console.log(++num10); -> 11
+// console.log(++num10); -> 12
+
+// console.log(num10--); -> 10
+// console.log(num10);  -> 9
+
+// console.log(--num10);
+// console.log(num10);
+
+
+
+// +=, -+, *=, /+. %=
+// var num20 = 20;
+
+// num20 = num20 + 10;
+// num20 += 10;
+
+// console.log(num20);
+
+
+
+// 비교연산자 >, <, ==, ===, <=, >=
+// var a = 10;
+// var b = 20;
+// var c = 30;
+
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// console.log(a == b);
+// console.log(a === b);
+
+// console.log(a != b);
+// console.log(a !== b);
+
+
+// var num10 = 10;
+// var num20 = 20;
+
+// var str10 = "10";
+// var str20 = "20";
+
+// console.log(num10 == str10);
+// console.log(num10 === str10);
+
+// console.log(num20 != str20);
+// console.log(num20 !== str20);
+
+
+
+// 논리 연산자 : AND (&&), OR(||)
+// var num30 = 30;
+// var num40 = 40;
+// var num50 = 30;
+
+// console.log(num30 == num40 && num30 === num50 && num40 != num50);
+// console.log(num30 == num40 || num30 === num50 || num40 > num50);
+
+
+
+// 불리언(boolean) : ture, false
+
+// console.log(true);
+// console.log(false);
+// console.log(1 === 2);
+// console.log(10 < 20);
+// console.log(10 === 10 || 20 === 30)
+
+// var bool = true
+// bool = false
