88a6cd7ffab795e82495b3a8cc782ab64b346d61
janeJeong <jyoon2005@naver.com>
2021-09-01 01:30:23 +0900

    JavaScript_base_(0830)

--- Web Programming/js-tutorial-2/js/main.js
+++ Web Programming/js-tutorial-2/js/main.js
@@ -317,3 +317,278 @@ var num2 = parseFloat(str2);
 console.log(num1);
 console.log(num2);
 */
+
+
+
+
+
+
+// 반복문
+// 고정값 : console.log(2 * );
+// 일정한 규직을 갖고 있는 가변값 : 1, 2, 3, 4, 5, 6, 7, 8, 9
+/*
+console.log(2 * 1);
+console.log(2 * 2);
+console.log(2 * 3);
+console.log(2 * 4);
+console.log(2 * 5);
+console.log(2 * 6);
+console.log(2 * 7);
+console.log(2 * 8);
+console.log(2 * 9);
+*/
+
+
+// while
+/*
+var num = 1;  // 2, 3, ... 10
+
+while (num < 10) {
+	// 2 * 1 = 2
+	// 2 * 2 = 4
+	// 2 * 3 = 6
+	// 2 * 9 = 18
+	console.log(2 * num)
+	num++;
+}
+
+console.log("반복문 종료");
+*/
+
+
+
+// do ~ while
+/*
+var i = 12;
+
+do {
+	console.log(i);
+	i++
+} while (i < 10);
+*/
+
+
+
+// 고정값
+// 일정한 규칙을 갖고 있는 가변값
+// for
+/*
+for (var i = 1; i < 10; i++) {  // i = 전역변수
+	console.log(2 * i);
+}
+*/
+
+
+
+// 중첩 반복문
+// 중첩 for문
+
+// 2단 ~ 9단
+/*
+for(var i = 2; i < 10; i++) {
+
+	for(var j = 1; j < 10; j++) {
+		console.log(i * j);
+	} 
+	// 안쪽에 반복문이 종료되면 바깥쪽이 새로운 가변값으로 초기값이 변경되면서 내부에 있는 for문이 초기값으로 변경된다.  
+}
+*/
+
+
+
+// break, continue 
+/*
+for (var i = 1; i < 10; i++) {
+
+	if(i === 5) {
+		break;   // 숫자 5가 되는 순간 반복을 멈춤, 아래쪽의 코드는 실행되지 x 
+	}
+
+	console.log(2 * i);
+}*/
+
+/*
+for (var i = 1; i < 10; i++) {
+
+	if(i === 5) {
+		continue;  // 숫자 5의 결과값은 스킵하고 진행
+	}
+
+	console.log(2 * i);
+}
+*/
+
+
+
+
+// 반복문 응용
+
+// var arr = ['바나나', '사과', '수박'];
+// console.log(arr);
+// console.log(arr.length);
+
+
+// 고정값 : console.log(arr[]) ;
+// 가변값 : 0, 1, 2
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+/*
+for (var i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+}
+*/
+
+
+
+// for ~ in
+/*
+for (var i in arr) {
+
+	console.log(i);
+	console.log(arr[i]);
+}
+*/
+
+
+/*
+arr.forEach( function(element, index) {
+
+	console.log(element);
+	console.log(index);
+
+})
+*/
+
+
+
+/*
+arr.map(function(element, index) {
+
+	console.log(element);
+	console.log(index);
+
+})
+*/
+
+/*
+var result = arr.map(function(element, index) {
+
+	return element + " " + "10";
+})
+
+console.log(result);
+*/
+
+
+
+
+
+// 객체 반복문 for ~ in
+/*
+var student = {
+	name: "Jane",
+	age: 23,
+	skills : ["자바스크립트", "HTML", "CSS"],
+	sum: function(num1, num2) {
+		console.log(num1 + num2);
+	}
+}
+
+
+// for ~ in
+// student.name  
+// student['name']  => 객체 안의 데이터에 접근하는 방법
+for (var prop in student) {   // prop = 키값, 데이터가 들어가는 좌표
+	console.log(prop);
+	console.log(student[prop]);
+}
+*/
+
+
+
+
+
+//문자열 properties, methods
+//length
+// var txtLength = "        Hello World           "
+
+// // console.log(txtLength.length);
+// console.log(txtLength);
+// console.log(txtLength.trim());
+
+/*
+var result = prompt("이름을 입력해 주세요");
+console.log(result);
+console.log(result.trim().length);
+*/
+
+
+// var str = "Nice to meet you to to";
+
+// console.log(str.length);
+// console.log(str.charAt(str.length - 1));
+// console.log(str.slice(2));
+
+// console.log(str.split('/'));
+
+
+// console.log(str.replace('Nice', 'Glad'));
+
+
+// console.log(str.indexOf('to'));
+
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+/*
+var search = prompt("검색어를 입력해 주세요.");
+
+if(search.toLowerCase() === "apple") {
+	console.log("사과 데이터를 출력한다.")
+} else {
+	console.log("false")
+}
+*/
+
+
+
+
+
+// 배열 properties, methods
+// var arr = ["사과", "배", "수박"]
+
+// console.log(arr.length);
+
+// arr.push("포도", "토마토");
+// console.log(arr);
+
+/*
+arr.unshift("포도");
+arr.unshift("토마토");  // 토마토가 배열의 맨 앞으로 감
+console.log(arr);
+*/
+
+
+/*
+arr.pop();
+arr.pop();
+console.log(arr);
+*/
+
+
+// arr.shift();
+// console.log(arr);
+
+
+/*
+var arr1 = ["사과", "배", "바나나"];
+var arr2 = ["노트북", "마이크", "키보드"];
+
+// var str1 = arr1.join(' / ');
+// console.log(str1)
+
+var merge = arr1.concat(arr2);  // 어떤 배열을 기준으로 합칠지 정해야 함
+console.log(merge);
+*/
